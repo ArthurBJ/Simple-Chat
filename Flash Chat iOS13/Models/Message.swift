8897cef130ed444cc500021b5f3dbdb7f0549b9a
ArthurBJ <baybikov.artur@mail.ru>
2023-08-08 19:23:56 +0300

    color separation among different users

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Артур Байбиков on 30.07.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
